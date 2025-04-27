1859e09ee77ba1f25ff1d5e20b5cd95f7c358a72
larryyang04 <larryyang2848@gmail.com>
2025-04-27 15:21:03 -0400

    edited routes and html to fix path for css

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from os import environ
 from dotenv import load_dotenv
+from app.routes import index, login, feed
 
 load_dotenv()
 
@@ -12,4 +13,8 @@ app.config["MONGO_URI"] = environ.get("MONGO_URI", "")
 app.config["SECRET_KEY"] = os.urandom(24)
 
 mongo = PyMongo(app)
-from app import routes
+
+# Register routes
+app.route("/")(index)
+app.route("/login", methods=["GET", "POST"])(login)
+app.route("/feed")(feed)
