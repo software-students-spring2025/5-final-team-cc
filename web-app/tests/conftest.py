bf26a077051212e86c60777d8be1f8f11c18cd47
James-Hou22 <james.hou04@gmail.com>
2025-04-28 17:05:10 -0400

    added config file for pytest and fixed typos

--- web-app/tests/conftest.py
+++ web-app/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from flask import Flask
-from app.routes import index, login, feed, post, logout # Need import all our route functions (might be missing a few)
+from app.routes import index, login, feed, create_post # Need import all our route functions (might be missing a few)
 
 def create_app():
     app = Flask(__name__)
@@ -10,8 +10,7 @@ def create_app():
     app.add_url_rule('/',         'index',  index)
     app.add_url_rule('/login',    'login',  login,   methods=['GET', 'POST'])
     app.add_url_rule('/feed',     'feed',   feed)
-    app.add_url_rule('/post',     'post',   post,    methods=['GET','POST'])
-    app.add_url_rule('/logout',   'logout', logout)
+    app.add_url_rule('/post',     'post',   create_post,    methods=['GET','POST'])
 
     return app
 
